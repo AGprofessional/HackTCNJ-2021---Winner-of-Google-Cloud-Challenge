ea7e098498ad6d99bbe6f6ce0706cdc78d0d27ff
Akash Gop <akash.gop3@gmail.com>
2021-02-27 10:16:11 -0500

    1st save in new branch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>New Branch</Text>
+      <Text>New Branch - new save</Text>
     
     </View>
   );
