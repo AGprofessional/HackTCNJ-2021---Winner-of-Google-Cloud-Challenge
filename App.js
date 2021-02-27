49aed97681f2b03d69ffbfccb88732d94f08044c
Akash Gop <akash.gop3@gmail.com>
2021-02-27 10:05:31 -0500

    made a new branch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Example App</Text>
+      <Text>New Branch</Text>
     
     </View>
   );
